a08f6b3c8078b2b2070bd61ee19f9294d92c1f76
Daniel <danielfcp34@gmail.com>
2025-08-06 21:44:18 -0500

    recupera del local y se devuelve a codigo

--- EJERCICIO 1/main.js
+++ EJERCICIO 1/main.js
@@ -8,5 +8,10 @@ class Producto {
     return (total = this.precio * this.cantidad);
   }
 }
+
+// 1
 const Producto1 = new Producto("zapato", 300, 2); //se crea el pbjeto
 localStorage.setItem("producto1", JSON.stringify(Producto1)); //se guarda en el local
+// 2
+const DatosGuardados = localStorage.getItem("producto1"); // se recupera del local
+const ObjetoPlano = JSON.parse(DatosGuardados); // se devuelve de formato JSON al normal
