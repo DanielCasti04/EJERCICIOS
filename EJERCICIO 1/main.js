c3f2339089baf9846e3a325644a5afd971973991
Daniel <danielfcp34@gmail.com>
2025-08-06 21:54:30 -0500

    Calcular Subtotal con el objeto retornado del Local

--- EJERCICIO 1/main.js
+++ EJERCICIO 1/main.js
@@ -5,15 +5,26 @@ class Producto {
     this.cantidad = cantidad;
   }
   CalcularSubtotal() {
-    return (total = this.precio * this.cantidad);
+    return this.precio * this.cantidad;
   }
 }
 
 // 1
 const Producto1 = new Producto("zapato", 300, 2); //se crea el pbjeto
 localStorage.setItem("producto1", JSON.stringify(Producto1)); //se guarda en el local
+
+const Producto2 = new Producto("manzana", 250, 7); //se crea el pbjeto
+localStorage.setItem("producto2", JSON.stringify(Producto2)); // se guarda en el local
+
 // 2
 const DatosGuardados = localStorage.getItem("producto1"); // se recupera del local
 const ObjetoPlano = JSON.parse(DatosGuardados); // se devuelve de formato JSON al normal
 
-console.log(ObjetoPlano); // se muestra en consola 
+console.log(ObjetoPlano); // se muestra en consola
+
+const ProductoDelLocal = new Producto(
+  ObjetoPlano.nombre,
+  ObjetoPlano.precio,
+  ObjetoPlano.cantidad
+);
+console.log(ProductoDelLocal.CalcularSubtotal());
